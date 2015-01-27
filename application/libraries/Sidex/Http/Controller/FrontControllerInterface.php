e889a568e5aa95352429ec42d4e4f7df2aa7e7bf
Jakim Hernández Arrieta <egalink@gmail.com>
2015-01-26 23:07:51 -0600

    The front controller interface was documented.

--- application/libraries/Sidex/Http/Controller/FrontControllerInterface.php
+++ application/libraries/Sidex/Http/Controller/FrontControllerInterface.php
@@ -2,16 +2,80 @@
 
 interface FrontControllerInterface {
 
+    /**
+     * Class constructor.
+     *
+     * @param  array $params (the array of configuration attributes).
+     * @return void
+     */
     public function __construct(array $params);
+
+    /**
+     * Takes an array of values to set as configuration attributes.
+     *
+     * @param  array $options (the array of values).
+     * @return void
+     */
     public function setConfiguration(array $options);
+
+    /**
+     * Set a value as configuration attribute.
+     *
+     * @param  string $attribute (the attribute name).
+     * @param  mixed  $value
+     * @return void
+     */
     public function setConfig($attribute, $value);
+
+    /**
+     * Gets the client request.
+     *
+     * @param  Sidex\Http\Request\Url Object in $Url
+     * @return void
+     */
     public function getClientRequest(\Sidex\Http\Request\Url $url);
+
+    /**
+     * Configures the client request.
+     *
+     * @param  string $uri (the requested uri.)
+     * @return void
+     */
     public function configureRequest($ruri);
+
+    /**
+     * Sets a controller name to be instantiated.
+     *
+     * @param  string $controller (the controller name.)
+     * @return Sidex\Http\Controller\FrontController Object
+     */
     public function setController($controller);
+
+    /**
+     * Sets the action name to be called from the controller.
+     *
+     * @param  string $action (the action name.)
+     * @return Sidex\Http\Controller\FrontController Object
+     */
     public function setAction($action);
+
+    /**
+     * Sets the parameters to be passed to the controller action, as an indexed
+     * array.
+     *
+     * @param  array  $params
+     * @return Sidex\Http\Controller\FrontController Object
+     */
     public function setParams(array $params);
+
+    /**
+     * Call to the controller action with an array of parameters.
+     *
+     * @return void
+     */
     public function run();
 
+    // end interface...
 }
 
 /* End of file FrontControllerInterface.php */
