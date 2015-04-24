e1ecc90ae7c36c1aa6cfa64fed4d543cce1673a9
Edgar Jakim Hernández Arrieta <egalink@gmail.com>
2015-04-24 11:01:56 -0500

    Creating the new View Class.

--- application/views/MainController/index.php
+++ application/views/MainController/index.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="description" content="<?php echo $title;?>">
-        <link href="<?php echo base_url('favicon.ico');?>" rel="shortcut icon">
+        <link href="<?php echo asset('favicon.ico');?>" rel="shortcut icon">
         <style type="text/css">
             * { margin: 0px; padding: 0px; }
 
